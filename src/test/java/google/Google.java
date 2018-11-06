f3f087fdf984d3b93db3e677586609327c547d5f
PotapIhor <potapenko.igorr@gmail.com>
2018-11-06 11:25:45 +0300

    new google searchv/ new2

--- src/test/java/google/Google.java
+++ src/test/java/google/Google.java
@@ -2,6 +2,7 @@ package google;
 
 
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -9,6 +10,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 class Google {
@@ -22,21 +24,36 @@ class Google {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        driver.get("http://google.com.ua");
-
     }
+
     @Test
-    public void userLogin() {
+    public void Find() {
+        driver.get("http://google.com.ua");
         WebElement search = driver.findElement(By.xpath("//*[@id=\"lst-ib\"]"));
-        search.sendKeys("Навуходоносор");
-        WebElement button = driver.findElement(By.xpath("/html/body/div/div[3]/form/div[2]/div[2]/div/div[2]/div[2]/div[1]/div/ul/li[11]/div/span[1]/span/input"));
-        button.click();
+        search.sendKeys("Nebuchadnezzar");
+        search.submit();
 
-
-        driver.findElement(By.tagName( "https://ru.wikipedia.org/wiki/")).click();
-
+        Assert.assertTrue(isPresent());
     }
 
+    public Boolean isPresent(){
+        List <WebElement> allElements = driver.findElements(By.linkText("https://en.wikipedia.org/wiki/Nebuchadnezzar_II"));
+        int s = allElements.size();
+        Boolean b = false;
+        if(s>0) {
+            b = true;
+        }
+        return b;
+    }
+
+
+
+       // driver.findElement(By.tagName( "https://ru.wikipedia.org/3/"))
+         //       .click();
+        //Assert.assertTrue();
+        //assert?
+
+//
     @AfterClass
     public static void tearDown() {
         driver.quit();
